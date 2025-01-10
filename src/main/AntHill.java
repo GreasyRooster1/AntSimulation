5ce40e2ea5281e0cedc41cd8c3dbf25f47d0bad2
GreasyRooster <greasyRooster1@gmail.com>
2025-01-10 11:17:39 -0800

    showing ants

--- src/main/AntHill.java
+++ src/main/AntHill.java
@@ -1,19 +1,28 @@
 package main;
 
+import static processing.core.PApplet.append;
+
 public class AntHill extends Entity {
     public Team team;
-    public Ant[] ant;
+    public Ant[] ants = {};
     public int countInside=0;
     public int countOutside=0;
 
     AntHill(float _x,float _y,Team _team){
         super(_x,_y,50,50);
         team = _team;
+        addAnts(50);
+    }
+
+    public void addAnts(int count){
+        for(int i=0;i<count;i++){
+            ants = (Ant[]) append(ants,new Ant(x,y,team));
+        }
     }
     public void update(){
         move();
-        updateAnts();
         draw();
+        updateAnts();
 
     }
     public void draw(){
@@ -23,7 +32,7 @@ public class AntHill extends Entity {
     public void updateAnts(){
         countInside=0;
         countOutside=0;
-        for(Ant a : ant){
+        for(Ant a : ants){
             if(a.inside) {
                 countInside++;
                 continue;
