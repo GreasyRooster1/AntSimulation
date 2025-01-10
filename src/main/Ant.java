d0325266a0df2cd8fed05e87ceb35b45dd996205
GreasyRooster <greasyRooster1@gmail.com>
2025-01-10 11:15:27 -0800

    update ants

--- src/main/Ant.java
+++ src/main/Ant.java
@@ -2,14 +2,20 @@ package main;
 
 public class Ant extends Entity{
     public Team team;
+    public boolean inside;
 
     Ant(float _x, float _y, Team _team) {
         super(_x, _y, 10, 10);
         team = _team;
+        inside = false;
     }
 
     public void draw(){
         Main.app.fill(team.getColor());
         Main.app.ellipse(x,y,w,h);
+    }
+
+    public void move(){
+
     }
 }
--- src/main/AntHill.java
+++ src/main/AntHill.java
@@ -3,16 +3,33 @@ package main;
 public class AntHill extends Entity {
     public Team team;
     public Ant[] ant;
+    public int countInside=0;
+    public int countOutside=0;
 
     AntHill(float _x,float _y,Team _team){
         super(_x,_y,50,50);
         team = _team;
     }
+    public void update(){
+        move();
+        updateAnts();
+        draw();
+
+    }
     public void draw(){
         Main.app.fill(team.getColor());
         Main.app.ellipse(x,y,w,h);
     }
-    public void move(){
-
+    public void updateAnts(){
+        countInside=0;
+        countOutside=0;
+        for(Ant a : ant){
+            if(a.inside) {
+                countInside++;
+                continue;
+            }
+            countOutside++;
+            a.update();
+        }
     }
 }
