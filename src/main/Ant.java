d85cf88aae7ac68d0be820d5b61a174753e50e94
GreasyRooster <greasyRooster1@gmail.com>
2025-01-10 11:07:44 -0800

    ants

--- src/main/Ant.java
+++ src/main/Ant.java
@@ -1,18 +1,15 @@
 package main;
 
-public class AntHill extends Entity {
+public class Ant extends Entity{
     public Team team;
-    public Ant[] ant;
 
-    AntHill(float _x,float _y,Team _team){
-        super(_x,_y,50,50);
+    Ant(float _x, float _y, Team _team) {
+        super(_x, _y, 10, 10);
         team = _team;
     }
+
     public void draw(){
         Main.app.fill(team.getColor());
         Main.app.ellipse(x,y,w,h);
-    }
-    public void move(){
-
     }
 }
